93b1cddad5a16150bd2695002ab0a128706e30bc
Eirik Gjerloew <egjerlow@gmail.com>
2011-08-22 15:06:15 +0200

    Can now read and write a MapData object with a single map to fits file

--- fileutils.py
+++ fileutils.py
@@ -13,15 +13,47 @@ def read_fits_map(fname):
     #subd = np.array(len(cols.names))
     nside = hdr['nside']
     npix = 12*nside*nside
-    map = np.zeros((subd, npix))
-    for i in range(subd):
-        map[i] = data.field(i).flatten()
-    #md.map = map
-    md = mapmod.MapData(nside, map=map, ordering=hdr['ordering'], 
+    md = mapmod.MapData(nside, ordering=hdr['ordering'], pol=hdr['polar'])
+    for i in range(md.map.shape[1]):
+        md.map[0, i] = data.field(i).flatten()
     hdulist.close()
     return md
 
 def write_fits_map(fname, md, bintab=True):
     #So far will only write information that is actually needed for this
     #library. Could cause compatibility problems later.
+    #So far only writes a single map with or without polarisation.
     if bintab:
+        if md.nside <= 8:
+            raise NotImplementedError("Nside must be at least 16 at this point")
+        npix = 12*md.nside**2
+        if npix % 1024 != 0:
+            raise NotImplementedError("npix must be a multiple of 1024")
+        numrows = npix // 1024
+        if md.pol:
+            if len(md.map.shape) != 3:
+                raise NotImplementedError()
+            col1 = pyfits.Column(name='TEMPERATURE', format='1024E', 
+                    array = md.map[0, 0].reshape(numrows, 1024))
+            col2 = pyfits.Column(name='Q-POLARISATION', format='1024E', 
+                    array = md.map[0, 1].reshape(numrows, 1024))
+            col3 = pyfits.Column(name='U-POLARISATION', format='1024E', 
+                    array = md.map[0, 2].reshape(numrows, 1024))
+            cols = pyfits.ColDefs([col1, col2, col3])
+        elif not md.pol:
+            if (len(md.map.shape)) != 2:
+                raise NotImplementedError()
+            col1 = pyfits.Column(name='TEMPERATURE', format='1024E',
+                                 array=md.map[0].reshape(numrows, 1024))
+            cols = pyfits.ColDefs([col1])
+        prihdu = pyfits.PrimaryHDU()
+        tbhdu = pyfits.new_table(cols)
+        thdr = tbhdu.header
+        thdr.update('NSIDE', md.nside, 'Resolution parameter for HEALPIX')
+        thdr.update('ORDERING', md.ordering.upper(), 
+                    'Pixel ordering scheme, either RING or NESTED')
+        thdr.update('POLAR', md.pol, 'Polarisation included (True/False)')
+        thdulist = pyfits.HDUList([prihdu, tbhdu])
+        thdulist.writeto(fname)
+    else:
+        raise NotImplementedError()
