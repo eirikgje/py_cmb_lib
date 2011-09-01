c4a748eb5d81fbd7d5c00592eda5500cddb018f0
Eirik Gjerlow <eirik.gjerlow@astro.uio.no>
2011-09-01 16:35:07 +0200

    Added test file for beammod

---
+++ test_pcl/test_beammod.py
@@ -0,0 +1,11 @@
+from __future__ import division
+import beammod
+import numpy as np
+from nose.tools import ok_, eq_, assert_raises
+import sys
+
+def test_gaussbeam():
+    beam = beammod.gaussbeam(10.0, 95, ndim=1)
+    yield eq_, ['TT'], beam.spectra
+    beam = beammod.gaussbeam(10.0, 95, ndim=4)
+    yield eq_, ['TT', 'EE', 'BB', 'TE'], beam.spectra
