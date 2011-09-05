39cb719fa9cd7050e19d9a6313e163a1676faf74
Eirik Gjerloew <egjerlow@gmail.com>
2011-09-05 22:59:00 +0200

    worked a bit further on implementing alm2map

--- utils.py
+++ utils.py
@@ -9,11 +9,17 @@ def alm2map(ad, nside):
     md = mapmod.MapData(nside, map=np.zeros(mshape), pol_axis=ad.pol_axis)
     ndims = len(mshape) - 1
     indlist = np.array(mshape[:md.indaxis] + mshape[md.indaxis + 1:])
-    currind = np.zeros(indlist, dtype=int)
-    for i in range(len(indlist)):
+    currind = np.zeros(ndims)
+    convalms = np.zeros((1, ad.lmax + 1, ad.mmax + 1), dtype=np.complex)
+    for i in range(ndims):
         for j in range(indlist[i]):
-            curralms = ad.alms[currind[:
-
-    #for i in range(ndims):
-    #    if i != md.indaxis:
-    #        for j in range(mshape[i]):
+            curralms = ad.alms[(currind[:nd.indaxis] + [Ellipsis,] +
+                                currind[nd.indaxis:])]
+            for l in range(ad.lmax + 1):
+                for m in range(ad.mmax + 1):
+                    convalms[1, l, m] = curralms[almmod.lm2ind([l, m])]
+            alm2map(nside, ad.lmax, ad.mmax, convalms, 
+                    md.map[currind[:nd.indaxis] + [Ellipsis,] + 
+                           currind[nd.indaxis:]]) 
+            currind[i] += 1
+            #TODO: Fix this
